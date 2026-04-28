e61ee446246f81ddecbffb23b8c7ac2c9d2afecd
Jaisuspr <Jesusrodz1406@gmail.com>
2026-04-27 21:17:50 -0400

    Create 9_palindrome_number.cs

---
+++ easy/9_palindrome_number.cs
@@ -0,0 +1,23 @@
+public class Solution
+{
+    public bool IsPalindrome(int x)
+    {
+    
+        if (x < 0)
+        {
+            return false;
+        }
+
+        int originalNumber = x;
+        int reversedNumber = 0;
+
+        while (x > 0)
+        {
+            int lastDigit = x % 10;
+            reversedNumber = (reversedNumber * 10) + lastDigit;
+            x = x / 10;
+        }
+
+        return originalNumber == reversedNumber;
+    }
+}
