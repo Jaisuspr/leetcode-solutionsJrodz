3142343e7041086e094a5d14fa4db11aa2d2f6ea
Jaisuspr <Jesusrodz1406@gmail.com>
2026-04-27 21:22:31 -0400

    Create 3_longest_substring.cs

---
+++ medium/3_longest_substring.cs
@@ -0,0 +1,23 @@
+public class Solution {
+    public int LengthOfLongestSubstring(string s) {
+        HashSet<char> set = new HashSet<char>();
+        int left = 0;
+        int maxLength = 0;
+
+        for (int right = 0; right < s.Length; right++) {
+            while (set.Contains(s[right])) {
+                set.Remove(s[left]);
+                left++;
+            }
+
+            set.Add(s[right]);
+
+            int length = right - left + 1;
+            if (length > maxLength) {
+                maxLength = length;
+            }
+        }
+
+        return maxLength;
+    }
+}
